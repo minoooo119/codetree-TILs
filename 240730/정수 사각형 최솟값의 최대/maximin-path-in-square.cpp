9ad07923a896515299a224f846d8649658113089
이민호 <65356142+minoooo119@users.noreply.github.com>
2024-07-30 14:56:23 +0900

    Title: 정수 사각형 최솟값의 최대, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240730/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
+++ 240730/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
@@ -3,61 +3,35 @@
 using namespace std;
 int n;
 int a[101][101];
-bool visited[101][101];
-int min_num=-1;
-
-int bfs(){
-    queue<pair<int,pair<int,int>>> q;
-    q.push({a[0][0],{0,0}});
-    // visited[0][0]=true;
-    int min_mx_num=-1;
-    while(!q.empty()){
-        int m=q.front().first;
-        int x=q.front().second.first;
-        int y=q.front().second.second;
-        visited[x][y]=true;
-        // cout<<x<<y<<'\n';
-        q.pop();
-        if(x==n-1&&y==n-1){
-            min_mx_num=max(min_mx_num,m);
-            continue;
-        }
-        if(x+1<n){
-            int mm=min(m,a[x+1][y]);
-            if(!visited[x+1][y]&&mm>min_mx_num) q.push({mm,{x+1,y}});
-        }
-        if(y+1<n){
-            int mm=min(m,a[x][y+1]);
-            if(!visited[x][y+1]&&mm>min_mx_num) q.push({mm,{x,y+1}});
-        }
-    }
-    return min_mx_num;
-}
-// void dfs(int x, int y, int num){
-//     if(x==n-1&&y==n-1){
-//         min_num=max(min_num,num);
-//         return;
-//     }
-//     if(x+1<n){
-
-//     }
-//     if(y+1<n){
-
-//     }
-// }
-
 int main() {
     // 여기에 코드를 작성해주세요.
     cin>>n;
-    int dp[101][101];
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             cin>>a[i][j];
-            // if(i==0&&j==0) dp[i][j]=a[i][j];
-            // else if(i==0) dp[i][j]=min(dp[i][j-1],a[i][j]);
-            // else if(j==0) dp[i][j]=min(dp[i-1][j],a[i][j]);
         }
     }
-    cout<<bfs()<<'\n';
+    int m3=10e8;
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1;j++){
+            m3=min(m3,a[i][j]);
+        }
+    }
+    int m3_1=min(m3,a[n-2][n-1]);
+    int m3_2=min(m3,a[n-1][n-2]);
+
+    int m1=10e8;
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n;j++){
+            m1=min(m1,a[i][j]);
+        }
+    }
+    int m2=10e8;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-1;j++){
+            m2=min(m2,a[i][j]);
+        }
+    }
+    cout<<max(m1,m2,m3_1,m3_2)<<'\n';
     return 0;
 }
