d21f4d80a58c8436320c4f8514266ec244b8dbd0
이민호 <65356142+minoooo119@users.noreply.github.com>
2024-07-30 15:15:33 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 68ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 240730/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
+++ 240730/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -7,8 +7,8 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin>>n;
-    int a[101][101];
-    int dp[101][101];
+    int a[501][501];
+    int dp[501][501];
 
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
