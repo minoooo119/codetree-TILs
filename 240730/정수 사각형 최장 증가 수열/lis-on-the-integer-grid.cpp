d0503f96adff6e0bb9a56ef85922b8f45b98a270
이민호 <65356142+minoooo119@users.noreply.github.com>
2024-07-30 15:44:36 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240730/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
+++ 240730/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -14,39 +14,37 @@ int main() {
     cin>>n;
     int a[501][501];
     int dp[501][501];
+    priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
 
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             cin>>a[i][j];
-            dp[i][j]=1;
+            dp[i][j]=0;
+            pq.push({a[i][j],{i,j}});
         }
-    }
-    while(1){
-        int change=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                for(int k=0;k<4;k++){
-                    int nx=i+dx[k];
-                    int ny=j+dy[k];
-                    if(nx<0||ny<0||nx>=n||ny>=n) continue;
-                    if(a[i][j]<=a[nx][ny]) continue;
-                    if(dp[i][j]>=dp[nx][ny]+1) continue;
-                    dp[i][j]=dp[nx][ny]+1;change++;
-                    
-                }
-            }
-        }
-        if(change==0) break;
     }
 
-    int mxNum=0;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            // cout<<dp[i][j]<<' ';
-            mxNum=max(mxNum,dp[i][j]);
+    while(!pq.empty()){
+        int x=pq.top().second.first;
+        int y=pq.top().second.second;
+        int val=pq.top().first;
+        pq.pop();
+        for(int i=0;i<4;i++){
+            int nx=x+dx[i];
+            int ny=y+dy[i];
+            if(a[nx][ny]>a[x][y]){
+                dp[nx][ny]=max(dp[nx][ny],dp[x][y]+1);
+            }
         }
-        // cout<<'\n';
     }
-    cout<<mxNum;
+    int mx_num=0;
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n;j++){
+    //         mx_num = max(mx_num,dp[i][j]);
+    //         cout<<dp[i][j]<<' ';
+    //     }
+    //     cout<<'\n';
+    // }
+    cout<<mx_num+1<<"\n";
     return 0;
 }
