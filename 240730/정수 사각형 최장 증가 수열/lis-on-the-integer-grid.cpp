4d261956b2852134e08ae310742ac11eddd69b7f
이민호 <65356142+minoooo119@users.noreply.github.com>
2024-07-30 15:13:42 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240730/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int dx[4]={0,1,0,-1};
+int dy[4]={1,0,-1,0};
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    int a[101][101];
+    int dp[101][101];
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+            dp[i][j]=1;
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            for(int k=0;k<4;k++){
+                int nx=i+dx[k];
+                int ny=j+dy[k];
+                if(nx<0||ny<0||nx>=n||ny>=n) continue;
+                if(a[i][j]>a[nx][ny]) dp[i][j]=max(dp[i][j],dp[nx][ny]+1);
+            }
+        }
+    }
+    int mxNum=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            // cout<<dp[i][j]<<' ';
+            mxNum=max(mxNum,dp[i][j]);
+        }
+        // cout<<'\n';
+    }
+
+    return 0;
+}
