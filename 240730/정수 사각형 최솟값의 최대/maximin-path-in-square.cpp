6bc89ce4df6aaee6a7028913fb625d1c9ffc5f2f
이민호 <65356142+minoooo119@users.noreply.github.com>
2024-07-30 15:00:36 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 10ms, Memory: 1MB, Status: Passed - CodeTree

--- 240730/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
+++ 240730/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
@@ -1,37 +1,45 @@
 #include <iostream>
-#include <bits/stdc++.h>
-using namespace std;
-int n;
-int a[101][101];
-int main() {
-    // 여기에 코드를 작성해주세요.
-    cin>>n;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cin>>a[i][j];
-        }
-    }
-    int m3=10e8;
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-1;j++){
-            m3=min(m3,a[i][j]);
-        }
-    }
-    int m3_1=min(m3,a[n-2][n-1]);
-    int m3_2=min(m3,a[n-1][n-2]);
+#include <algorithm>
 
-    int m1=10e8;
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n;j++){
-            m1=min(m1,a[i][j]);
-        }
-    }
-    int m2=10e8;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-1;j++){
-            m2=min(m2,a[i][j]);
-        }
-    }
-    cout<<max(m1,max(m2,max(m3_1,m3_2)))<<'\n';
+#define MAX_NUM 1000
+
+using namespace std;
+
+int n;
+int num[MAX_NUM][MAX_NUM];
+int dp[MAX_NUM][MAX_NUM];
+
+void Initialize() {
+    // 시작점의 경우 dp[0][0] = num[0][0]으로 초기값을 설정해줍니다
+    dp[0][0] = num[0][0];
+
+    // 최좌측 열의 초기값을 설정해줍니다.
+    for(int i = 1; i < n; i++)
+        dp[i][0] = min(dp[i-1][0], num[i][0]);
+
+    // 최상단 행의 초기값을 설정해줍니다.
+    for(int j = 1; j < n; j++)
+        dp[0][j] = min(dp[0][j-1], num[0][j]);
+}
+
+int main() {
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> num[i][j];
+
+    // 초기값 설정
+    Initialize();
+
+    // 탐색하는 위치의 위에 값과 좌측 값 중에 큰 값과
+    // 해당 위치의 숫자 중에 최솟값을 구해줍니다.
+    for(int i = 1; i < n; i++)
+        for(int j = 1; j < n; j++)
+            dp[i][j] = min(max(dp[i-1][j], dp[i][j-1]), num[i][j]);
+
+
+    cout << dp[n-1][n-1];
+
     return 0;
 }
