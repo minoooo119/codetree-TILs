f3f9f403bbfec8be8a617b5f44e8bea6054e7bf2
이민호 <65356142+minoooo119@users.noreply.github.com>
2024-07-31 10:43:35 +0900

    Title: 뿌요뿌요, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240731/뿌요뿌요/puyo-puyo.cpp
+++ 240731/뿌요뿌요/puyo-puyo.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int box[101][101];
 int n;
-int block_num=0;
+int block_num=1;
 bool CanGo(int x, int y, int num){
     if(x<0||y<0||x>=n||y>=n) return false;
     if(box[x][y]!=num) return false;
@@ -39,8 +39,10 @@ int main() {
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             if(box[i][j]){
-                block_num=0;
-                dfs(i,j,box[i][j]);
+                block_num=1;
+                int num=box[i][j];
+                box[i][j]=0;
+                dfs(i,j,num);
                 if(block_num>=4) boom_num++;
                 max_block_num=max(max_block_num,block_num);
             }
