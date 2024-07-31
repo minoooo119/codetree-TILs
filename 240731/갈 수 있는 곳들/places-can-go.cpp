cc1313afa197af4e17af9fc3dfd7b4d2857d60b9
이민호 <65356142+minoooo119@users.noreply.github.com>
2024-07-31 11:22:16 +0900

    Title: 갈 수 있는 곳들, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240731/갈 수 있는 곳들/places-can-go.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int grid[101][101];
+int visited[101][101];
+int n,m;
+bool CanGo(int x, int y){
+    if(x<0||y<0||x>=n||y>=n) return false;
+    if(visited[x][y]||grid[x][y]==1) return false;
+    return true;
+}
+queue<pair<int,int>> q;
+int num=1;
+int BFS(){
+    while(!q.empty()){
+        int x=q.front().first;
+        int y=q.front().second;
+        q.pop();
+        int dx[4]={1,0,-1,0};
+        int dy[4]={0,1,0,-1};
+        for(int i=0;i<4;i++){
+            int nx=x+dx[i];
+            int ny=y+dy[i];
+            if(CanGo(nx,ny)){
+                // cout<<"push";
+                num++;
+                visited[nx][ny]=1;
+                q.push({nx,ny});
+            }
+        }
+
+    } 
+    // cout<<"fin\n";
+    return num;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>grid[i][j];
+            visited[i][j]=0;
+        }
+    }
+    vector<pair<int,int>> p;
+    for(int i=0;i<m;i++){
+        int a,b;
+        cin>>a>>b;
+        q.push({a,b});
+        visited[a][b]=1;
+    }
+    cout<<BFS();
+    return 0;
+}
