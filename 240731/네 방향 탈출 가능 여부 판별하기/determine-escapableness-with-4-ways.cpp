200b077da1d8ddde04dc26e18de3b5ed9275cf13
이민호 <65356142+minoooo119@users.noreply.github.com>
2024-07-31 11:17:39 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240731/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
+++ 240731/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 int grid[101][101];
 int visited[101][101];
-int n;
+int n,m;
 bool CanGo(int x, int y){
-    if(x<0||y<0||x>=n||y>=n) return false;
+    if(x<0||y<0||x>=n||y>=m) return false;
     if(visited[x][y]||grid[x][y]==0) return false;
     return true;
 }
@@ -14,28 +14,30 @@ bool BFS(){
     while(!q.empty()){
         int x=q.front().first;
         int y=q.front().second;
-        if(x==n-1&&y==n-1) return true;
         q.pop();
+        if(x==n-1&&y==m-1) return true;
         int dx[4]={1,0,-1,0};
         int dy[4]={0,1,0,-1};
         for(int i=0;i<4;i++){
             int nx=x+dx[i];
             int ny=y+dy[i];
             if(CanGo(nx,ny)){
+                // cout<<"push";
                 visited[nx][ny]=1;
                 q.push({nx,ny});
             }
         }
 
     } 
+    // cout<<"fin\n";
     return false;
 }
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    cin>>n;
+    cin>>n>>m;
     for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+        for(int j=0;j<m;j++){
             cin>>grid[i][j];
             visited[i][j]=0;
         }
