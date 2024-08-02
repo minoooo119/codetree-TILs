1263600c26a44ae3cccbe073d4853135560253f1
이민호 <65356142+minoooo119@users.noreply.github.com>
2024-08-02 14:54:41 +0900

    Title: 겹치지 않게 선분 고르기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240802/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
+++ 240802/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
@@ -1,59 +1,66 @@
-#include <bits/stdc++.h>
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <tuple>
+
+#define MAX_N 15
+
 using namespace std;
+
 int n;
-vector<pair<int,int> > v;
-vector<pair<int,int> > res;
-int visited[16];
+pair<int, int> segments[MAX_N];
 
-int check_line(){
-    // int x=res[0].first;
-    int y=res[0].second;
-    int result=1;
-    for(int i=1;i<res.size();i++){
-        if(res[i].first>y) result++;
-        y=res[i].second;
-    }
+int ans;
+vector<pair<int, int> > selected_segs;
 
-    return result;
+bool Overlapped(pair<int, int> seg1, pair<int, int> seg2) {
+    int ax1, ax2;
+    tie(ax1, ax2) = seg1;
+
+    int bx1, bx2;
+    tie(bx1, bx2) = seg2;
+
+    // 두 선분이 겹치는지 여부는
+    // 한 점이 다른 선분에 포함되는 경우로 판단 가능합니다. 
+    return (ax1 <= bx1 && bx1 <= ax2) || (ax1 <= bx2 && bx2 <= ax2) ||
+           (bx1 <= ax1 && ax1 <= bx2) || (bx1 <= ax2 && ax2 <= bx2);
 }
-int mx=0;
-void backtracking(int cnt){
-    if(cnt == n){    
-        // for(auto p: res){
-        //     cout<<p.first<<' '<<p.second<<"\n";
-        // }
-        // return;
-        mx=max(mx,check_line());
-        return ;
-    }
-    for(int i=0;i<v.size();i++){
-        if(!visited[i]){
-            // cout<<"=>>"<<i<<": "<<v[i].first<<' '<<v[i].second<<" cnt: "<<cnt<<"\n";
-            visited[i]=1;
-            res.push_back(v[i]);
-            backtracking(cnt+1);
-            res.pop_back();
-            visited[i]=0;
-        }
-        // else{
-        //     cout<<"중복 금지"<<i<<": "<<v[i].first<<' '<<v[i].second<<" cnt: "<<cnt<<"\n";
-        // }
-    }
+
+bool Possible() {
+    // 단 한쌍이라도 선분끼리 겹치면 안됩니다.
+	for(int i = 0; i < (int)selected_segs.size(); i++)
+		for(int j = i + 1; j < (int)selected_segs.size(); j++)
+            if(Overlapped(selected_segs[i], selected_segs[j]))
+                return false;
+
+	return true;
+}
+
+void FindMaxSegments(int cnt) {
+	if(cnt == n) {
+		if(Possible())
+			ans = max(ans, (int) selected_segs.size());
+		return;
+	}
+    
+	selected_segs.push_back(segments[cnt]);
+	FindMaxSegments(cnt + 1);
+	selected_segs.pop_back();
+    
+	FindMaxSegments(cnt + 1);
 }
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    cin>>n;
-    for(int i=0;i<n;i++){
-        int a, b;
-        cin>>a>>b;
-        v.push_back(make_pair(a,b));
-        visited[i]=0;
+	cin >> n;
+	
+	for(int i = 0; i < n; i++) {
+        int x1, x2;
+        cin >> x1 >> x2;
+        segments[i] = make_pair(x1, x2);
     }
-    // cout<<"-------------------\n";
-    backtracking(0);
-    cout<<mx;
 
-    return 0;
+	FindMaxSegments(0);
+	
+	cout << ans;
+	return 0;
 }
